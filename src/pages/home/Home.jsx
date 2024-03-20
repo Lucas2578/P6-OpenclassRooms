fcc97f8bad550f56803f1aa4abfe4ba2ed70a539
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-03-20 21:17:49 +0100

    ✨ Add banner components and scss

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../../components/header/Header';
+import Banner from '../../components/banner/Banner';
+import imgHomeBanner from '../../assets/images/banner/home_banner.png';
 
 const Home = () => {
   return (
     <div className="container">
       <Header />
+      <Banner image={imgHomeBanner} texte="Chez vous, partout et ailleurs"/>
     </div>
   );
 };

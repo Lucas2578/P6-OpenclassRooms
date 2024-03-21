206156e26912b0fb5b5a182ce50efd8593337c4e
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-03-21 12:52:33 +0100

    ✨ Add NoPageFound desktop format

--- src/pages/errors/NoPageFound.jsx
+++ src/pages/errors/NoPageFound.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 
-const NoPageFound = () => {
+const NoPageFound = ({ errorNumber, errorText }) => {
   return (
     <div>
-      <h1>Accueil</h1>
-      <p>ERREUR 404</p>
+      <div className="container">
+        <Header />
+        <main className="error">
+          <h2 className="error__number">404</h2>
+          <p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
+          <a className="error__back" href="#/">Retourner sur la page d'accueil</a>
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 };

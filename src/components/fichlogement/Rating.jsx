e5238a2cc3fe6810452000d3ab8c418ae46a2f0d
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-03-22 11:26:55 +0100

    🐛 Added condition to display a full star if index is less than or EQUAL to score

--- src/components/fichlogement/Rating.jsx
+++ src/components/fichlogement/Rating.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 function Rating(props) {
     const score = props.score;
+    // Initialisation d'un tableau "notes"
     const notes = [1, 2, 3, 4, 5];
 
     return (
-        <div className="logement__description-info--rating">
+        <ul className="logement__description-info--rating">
             {notes.map((index) => (
-                // Ici on compare l'index au score, si l'index (qui correspond au numéro de l'étoile) est plus petit que le score, alors, cela ajoutera la 
+                // Ici on compare l'index au score, si l'index (qui correspond au numéro de l'étoile) est plus petit ou égal que le score, alors, cela ajoutera la 
                 // class "full-star", sinon, cela ajoutera "empty-star"
-                <i key={index} className={`fa-solid fa-star ${index < score ? 'full-star' : 'empty-star'}`}></i>
+                <li key={index} className={`fa-solid fa-star ${index <= score ? 'full-star' : 'empty-star'}`}></li>
             ))}
-        </div>
+        </ul>
     );
 }
 

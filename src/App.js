de0067df992d74d369f01f1d2f4161d8fb00a1fd
Lucas2578 <106094083+Lucas2578@users.noreply.github.com>
2024-03-27 15:20:39 +0100

    🔧 Remove HashRooter and add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./styles/app.scss";
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/home/Home';
 import NoPageFound from './pages/errors/NoPageFound';
 import About from './pages/about/About';
@@ -8,14 +8,14 @@ import FichLogement from './pages/logements/FichLogement';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/" element={<Home />} />
         <Route path="/logement/:id" element={<FichLogement />} />
         <Route path ="*" element={<NoPageFound />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
